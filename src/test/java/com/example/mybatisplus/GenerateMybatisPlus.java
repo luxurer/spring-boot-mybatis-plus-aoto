93c8711ddee952416d418c3193f6701f3d081660
luxurer <1161787404>
2020-01-06 14:16:59 +0800

    第6次提交代码

--- src/test/java/com/example/mybatisplus/GenerateMybatisPlus.java
+++ src/test/java/com/example/mybatisplus/GenerateMybatisPlus.java
@@ -67,7 +67,7 @@ public class GenerateMybatisPlus {
         // 包配置
         PackageConfig pc = new PackageConfig();
         //pc.setModuleName(scanner("模块名"));
-        pc.setParent("com.example.mybatisplus");
+        pc.setParent("com.ltt.demo");
         mpg.setPackageInfo(pc);
 
         // 自定义配置
